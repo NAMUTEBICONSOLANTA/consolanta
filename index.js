f1552bb90f8ea429af8702bc2ad907c7e6ec0426
NAMUTEBICONSOLANTA <146157021+NAMUTEBICONSOLANTA@users.noreply.github.com>
2023-12-07 12:33:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("demo").innerHTML = "The life of art as friends gather together in one accord calling for togetherness with the most splending art!"
+alert("Don't dare miss this!")
+document.getElementById("demo1").innerHTML = "When love binds together, fashion brings your joy. that love that will never let us down is the fashion that brings you harmony! "
+document.getElementById("header").innerHTML = "Your beauty will always be depend on your look and with the best designer, you will achieve the beauty goal!"
+document.getElementById("demo2").innerHTML = "Your age never determines your look with the Sheila Pink fashions but your fashion gives you the age!"
+
